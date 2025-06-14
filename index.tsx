090ae8149fa5a2187a5486c9d71fc385cb1de91f
Nate Maile <npmaile@gmail.com>
2025-06-13 22:07:17 -0400

    test react stuff

--- index.tsx
+++ index.tsx
@@ -1,3 +1,20 @@
+import { hydrateRoot } from 'react-dom/client';
+import { Component } from 'react';
+document.body.innerHTML = '<div id="app"></div>';
 import imgUrl from './photos/og-photo.jpg?w=300&h=300'
 document.getElementById('meta-image')!.setAttribute("content",imgUrl);
+
+
+class Abc extends Component {
+	render(){
+		return <div>information will come soon</div>
+	}
+}
+
+const root = hydrateRoot(document.getElementById('app'), (<h1>
+							  Nate and Susy wedding page
+							  <Abc/>
+							  <img src={imgUrl}></img>
+							  </h1>));
+
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite"
 import { imagetools } from "vite-imagetools"
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [
-		imagetools()
+		imagetools(),react()
 	]
 })
