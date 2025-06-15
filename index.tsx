cba1fd87049f8153e7828b0c35eef6008bea14a5
Nate Maile <npmaile@gmail.com>
2025-06-14 22:45:47 -0400

    stuff

--- index.tsx
+++ index.tsx
@@ -1,120 +1,17 @@
 import { hydrateRoot } from 'react-dom/client';
-import { Component } from 'react';
+import React, { Component } from 'react';
 document.body.innerHTML = '<div id="app"></div>';
 import imgUrl from './photos/og-photo.jpg?w=300&h=300'
-document.getElementById('meta-image')!.setAttribute("content",imgUrl);
+document.getElementById('meta-image')!.setAttribute("content", imgUrl);
 import { Header } from './src/header';
-import {ParallaxProvider} from 'react-scroll-parallax';
-import {ScrollingBackground} from './src/background';
-
-
-class Abc extends Component {
-	render(){
-		return <div class="text-5xl font-bold">information will come soon</div>
-	}
-}
+import { ParallaxProvider } from 'react-scroll-parallax';
+import { ScrollingBackground } from './src/background';
 
 const root = hydrateRoot(document.getElementById('app'),
-			 	<>
-<ParallaxProvider>
-<ScrollingBackground>
-</ScrollingBackground>
-				<Header></Header>
-			 	<h1>
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					Nate and Susy wedding page
-					<Abc/>
-					<img src={imgUrl}></img>
-				</h1>
-				</ParallaxProvider>
-				</>
-			);
+	<>
+		<ParallaxProvider>
+			<ScrollingBackground />
+			<Header />
+		</ParallaxProvider>
+	</>
+);
--- src/background/index.tsx
+++ src/background/index.tsx
@@ -1,19 +1,17 @@
-import {Parallax} from 'react-scroll-parallax';
+import React from 'react';
+import { Parallax } from 'react-scroll-parallax';
 
-export function ScrollingBackground(){
-	return(
-		<div class="static">
-		<Parallax speed={-10}>
-		<img src="/og-photo.jpg"></img>
-		<img src="/og-photo.jpg"></img>
-		<img src="/og-photo.jpg"></img>
-		<img src="/og-photo.jpg"></img>
-		<img src="/og-photo.jpg"></img>
-		<img src="/og-photo.jpg"></img>
-		<img src="/og-photo.jpg"></img>
-		<img src="/og-photo.jpg"></img>
-		<img src="/og-photo.jpg"></img>
-		</Parallax>
+export function ScrollingBackground() {
+	return (
+		<div className="absolute z-0">
+			<Parallax speed={100}>
+				<img src="/og-photo.jpg"></img>
+				<img src="/og-photo.jpg"></img>
+				<img src="/og-photo.jpg"></img>
+				<img src="/og-photo.jpg"></img>
+				<img src="/og-photo.jpg"></img>
+				<img src="/og-photo.jpg"></img>
+			</Parallax>
 		</div>
 	);
 }
