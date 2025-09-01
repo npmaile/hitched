cab2ca236280e042ad5412c74d81e0ed7dfd658e
Nate Maile <npmaile@gmail.com>
2025-09-01 01:25:00 -0400

    add PDF

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite"
-import { imagetools } from "vite-imagetools"
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-	plugins: [
-		imagetools(),react()
-	]
-})
